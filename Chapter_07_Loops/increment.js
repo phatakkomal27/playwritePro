95722626e63a1e5d05c29a7ba449863da697b76e
Komal Phatak <phatakkomal27@gmail.com>
2026-02-27 08:13:57 +0530

    Save workspace changes (auto commit)

---
+++ Chapter_07_Loops/increment.js
@@ -0,0 +1,13 @@
+// Increment Operator
+// ++ -> increase the value 1
+// Pre -   ()
+ let a = 10;
+ let b = ++a; // (++a ->  10+1)
+ console.log(b);
+console.log(a);
+
+
+ let aa = 10;
+ let bb = aa++;
+console.log(bb); //10
+ console.log(aa); //11
